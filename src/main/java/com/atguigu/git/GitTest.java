317f635e0b84c0f032786eb708a715ad6fd06513
myyyds <88066666+myyyds@users.noreply.github.com>
2021-07-31 16:37:52 +0800

    pull commit22

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("pull test2!");
-        System.out.println("pull test3!");
+        System.out.println("pull test22!");
     }
 }
